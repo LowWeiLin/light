4925039d3d83de88fdd53ed7e0e58997ea37039d
WL <lwl1991@gmail.com>
2022-07-06 13:54:07 +0000

    Simplex noise

--- components/r3f/hexagon/HexagonGrid.tsx
+++ components/r3f/hexagon/HexagonGrid.tsx
@@ -1,23 +1,30 @@
 import { useMemo } from "react";
 import { defineGrid, extendHex } from "honeycomb-grid";
+import SimplexNoise from "simplex-noise";
 import Hexagon from "./Hexagon";
+import { useFrame } from "@react-three/fiber";
 
 const Hex = extendHex({
   orientation: "pointy",
 });
 const Grid = defineGrid(Hex);
+const simplex = new SimplexNoise();
 
 const HexagonGrid = () => {
   const grid = useMemo(() => Grid.hexagon({ radius: 16 }), []);
 
   return (
     <>
-      {grid.map((hex) => (
-        <Hexagon
-          key={`${hex.x},${hex.y}`}
-          position={[hex.toPoint().x, 0, hex.toPoint().y]}
-        />
-      ))}
+      {grid.map((hex) => {
+        const point = hex.toPoint();
+        const height = simplex.noise2D(point.x * 0.1, point.y * 0.1);
+        return (
+          <Hexagon
+            key={`${hex.x},${hex.y}`}
+            position={[point.x, height, point.y]}
+          />
+        );
+      })}
     </>
   );
 };
