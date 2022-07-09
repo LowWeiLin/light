43119ed5a0ed9973741a3f18080ac9ce4e0f1667
WL <lwl1991@gmail.com>
2022-07-09 16:36:36 +0800

    Fix files for prettier

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpackDevMiddleware: config => {
+  webpackDevMiddleware: (config) => {
+    // eslint-disable-next-line no-param-reassign
     config.watchOptions = {
       poll: 1000,
       aggregateTimeout: 300,
-      ignored: '**/node_modules',
-    }
-    return config
+      ignored: "**/node_modules",
+    };
+    return config;
   },
 };
 
